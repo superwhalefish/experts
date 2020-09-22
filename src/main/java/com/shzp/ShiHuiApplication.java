0a0650f21fce1e706fde388e2859110efe91dfa9
Administrator <Administrator@WIN-8I8OA6OUHMP>
2020-09-22 17:09:16 +0800

    搭建测试

---
+++ src/main/java/com/shzp/ShiHuiApplication.java
@@ -0,0 +1,17 @@
+package com.shzp;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@MapperScan("com.shzp.dao")
+@EnableAsync
+public class ShiHuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiHuiApplication.class, args);
+	}
+
+}
